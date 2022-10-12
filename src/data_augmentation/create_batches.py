db8a234961e54863e7da27a25832cbdb701ddf60
gdeshpande9 <66385690+gdeshpande9@users.noreply.github.com>
2022-10-11 23:52:52 -0400

    added better return condition for create batches

--- src/data_augmentation/create_batches.py
+++ src/data_augmentation/create_batches.py
@@ -3,9 +3,9 @@ import numpy as np
 import shutil
 
 def create_batches(videos, num_batches, output_path):
-    if os.path.exists(output_path):
-        shutil.rmtree(output_path)
-    
+    if os.path.exists(f'{output_path}/batch_{num_batches}.txt'):
+        return
+    shutil.rmtree(output_path)
     os.makedirs(output_path)
     batches = np.array_split(np.array(videos), num_batches)
     for i, batch in enumerate(batches):
