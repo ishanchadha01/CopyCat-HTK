78cf4ec055ee8bed171173ec6231f2d966c7ed30
Ishan Chadha <ishanchadha01@gmail.com>
2022-12-14 15:04:13 -0500

    tkinter gui working with getting modified annotation data in same format as original annotation data

--- visualization/run_elan.py
+++ visualization/run_elan.py
@@ -11,49 +11,52 @@ class ElanGui:
   def __init__(self, video_filepath=""):
     self.window = tk.Tk()
     self.window.geometry("800x400")
-
+    self.elan_exec = ""
+    self.video_fp_list = []
+    self.video_fp_list_idx = 0
+    self.boundary_annotations = {}
+    self.eaf_savedir = ""
+    self.edited_annotations = {}
 
     elan_exec_text = tk.StringVar()
     elan_exec_text.set("Path to ELAN executable")
-    elan_exec_label = tk.Label(self.window, textvariable=elan_exec_text, height=2)
-    elan_exec_label.grid(row=1, column=1)
+    self.elan_exec_label = tk.Label(self.window, textvariable=elan_exec_text, height=2)
+    self.elan_exec_label.grid(row=1, column=1)
     self.elan_exec_textbox = tk.Text(self.window, height=2, width=40)
     self.elan_exec_textbox.insert("end", "/home/ishan/Documents/research/ccg/elan/bin/ELAN")
     self.elan_exec_textbox.grid(row=1, column=2)
 
-
     mlf_fp_text = tk.StringVar()
     mlf_fp_text.set("MLF filepath name for annotations")
-    mlf_fp_label = tk.Label(self.window, textvariable=mlf_fp_text, height=2)
-    mlf_fp_label.grid(row=2, column=1)
+    self.mlf_fp_label = tk.Label(self.window, textvariable=mlf_fp_text, height=2)
+    self.mlf_fp_label.grid(row=2, column=1)
     self.mlf_textbox = tk.Text(self.window, height=2, width=40)
     self.mlf_textbox.insert("end", "/home/ishan/Documents/research/ccg/copycat/CopyCat-HTK/projects/MediaPipe/results/res_hmm5.mlf")
     self.mlf_textbox.grid(row=2, column=2)
 
-
     video_dir_text = tk.StringVar()
     video_dir_text.set("Video directory name")
-    video_dir_label = tk.Label(self.window, textvariable=video_dir_text, height=2)
-    video_dir_label.grid(row=3, column=1)
+    self.video_dir_label = tk.Label(self.window, textvariable=video_dir_text, height=2)
+    self.video_dir_label.grid(row=3, column=1)
     self.video_dir_textbox = tk.Text(self.window, height=2, width=40)
     self.video_dir_textbox.insert("end", "/home/ishan/Documents/research/ccg/copycat/DATA/input")
     self.video_dir_textbox.grid(row=3, column=2)
 
-
     eaf_savedir_text = tk.StringVar()
     eaf_savedir_text.set("Elan annotation file save directory")
-    eaf_savedir_label = tk.Label(self.window, textvariable=eaf_savedir_text, height=2)
-    eaf_savedir_label.grid(row=4, column=1)
+    self.eaf_savedir_label = tk.Label(self.window, textvariable=eaf_savedir_text, height=2)
+    self.eaf_savedir_label.grid(row=4, column=1)
     self.eaf_savedir_textbox = tk.Text(self.window, height=2, width=40)
     self.eaf_savedir_textbox.insert("end", "/home/ishan/Documents/research/ccg/elan")
     self.eaf_savedir_textbox.grid(row=4, column=2)
 
-    self.next_video_button = tk.Button(self.window, text="Start Video Loop", command=self.launch_elan)
-    self.next_video_button.grid(row=5, column=2)
+    self.start_loop_button = tk.Button(self.window, text="Start Video Loop", command=self.launch_elan)
+    self.start_loop_button.grid(row=5, column=2)
+
+    self.next_video_button = tk.Button(self.window, text="Next Video", command=self.next_video)
 
     self.close_button = tk.Button(self.window, text="Quit", command=self.window.destroy)
     self.close_button.grid(row=6, column=2)
-
 
     self.window.mainloop()
 
@@ -61,55 +64,68 @@ class ElanGui:
   def launch_elan(self):
 
     # Initialize next video button, read textboxes after that
-    self.is_elan_launched = True
-    next_video_button_clicked = tk.IntVar()
-    self.next_video_button.configure(text="Next Video")
+    self.start_loop_button.grid_forget()
 
     # Get path to ELAN executable
-    elan_exec = self.elan_exec_textbox.get("1.0", "end-1c")
-    self.elan_exec_textbox.pack_forget()
+    self.elan_exec = self.elan_exec_textbox.get("1.0", "end-1c")
+    self.elan_exec_label.grid_forget()
+    self.elan_exec_textbox.grid_forget()
 
     # Get MLF filepath
     mlf_fp = self.mlf_textbox.get("1.0", "end-1c")
-    annotations = mlf_to_dict(mlf_fp)
-    self.mlf_textbox.pack_forget()
+    self.boundary_annotations = mlf_to_dict(mlf_fp)
+    self.mlf_fp_label.grid_forget()
+    self.mlf_textbox.grid_forget()
 
     # Get all videos to iterate over. TODO: Have options for selecting specific videos or something
     video_dir = self.video_dir_textbox.get("1.0", "end-1c")
-    video_fps = []
     for root, dirs, files in os.walk(video_dir):
       for video_fp in files:
         if video_fp.endswith(".mp4"):
-          video_fps.append(os.path.join(root, video_fp))
-    self.video_dir_textbox.pack_forget()
+          self.video_fp_list.append(os.path.join(root, video_fp))
+    first_video = self.video_fp_list[0]
+    video_len = int(float(FFProbe(first_video).video[0].duration) * 1000)
+    self.boundary_annotations = scale_annotations(self.boundary_annotations, video_len)
+    self.video_dir_label.grid_forget()
+    self.video_dir_textbox.grid_forget()
 
     # Get EAF savedir
-    eaf_savedir = self.eaf_savedir_textbox.get("1.0", "end-1c")
-    self.eaf_savedir_textbox.pack_forget()
+    self.eaf_savedir = self.eaf_savedir_textbox.get("1.0", "end-1c")
+    self.eaf_savedir_label.grid_forget()
+    self.eaf_savedir_textbox.grid_forget()
 
-    # Iterate through videos
-    for video_fp in video_fps: 
-      video_len = int(float(FFProbe(video_fp).video[0].duration) * 1000)
-      annotations = scale_annotations(annotations, video_len)
-
-      eaf_obj = make_elan(annotations, has_states=True, video_dirs=[video_fp], \
-        eaf_savedir=eaf_savedir)[0]
-      
-      eaf_path = os.path.join(eaf_savedir, os.path.basename(os.path.splitext(video_fp)[0])) + ".eaf"
-      if platform.system() == "Linux":
-        os.system(elan_exec + " " + eaf_path)
-        # subprocess.call(["xdg-open", eaf_path])
-      elif platform.system == "Darwin":
-        # TODO: use open
-        pass
-      elif platform.system == "Windows":
-        os.startfile(eaf_path)
-
-      # Wait for button to be clicked again before proceeding to next video
-      self.next_video_button.wait_variable(next_video_button_clicked)
-      next_video_button_clicked.set(0)
+    self.next_video_button.grid(row=1, column=1)
 
 
+  def next_video(self):
+    # Process video at current index
+    video_fp = self.video_fp_list[self.video_fp_list_idx]
+    eaf_obj = make_elan(self.boundary_annotations, has_states=True, video_dirs=[video_fp], \
+      eaf_savedir=self.eaf_savedir)[0]
+    eaf_path = os.path.join(self.eaf_savedir, os.path.basename(os.path.splitext(video_fp)[0])) + ".eaf"
+
+    # TODO: test on different OSs, possibly remove conditional
+    if platform.system() == "Linux":
+      os.system(self.elan_exec + " " + eaf_path)
+    elif platform.system == "Darwin":
+      os.system(self.elan_exec + " " + eaf_path)
+    elif platform.system == "Windows":
+      os.system(self.elan_exec + " " + eaf_path)
+
+    self.process_video(eaf_obj)
+    self.video_fp_list_idx += 1
+
+
+  def process_video(self, eaf_obj):
+    # Once the video has been closed out of, reexamine EAF for new boundaries
+    tier_names = eaf_obj.get_tier_names()
+    new_annotations = {}
+    for tier in list(tier_names):
+      new_annotations[tier] = {}
+      for start, end, state in eaf_obj.get_annotation_data_for_tier(tier):
+        mew_annotations[tier][state] = [start, end]
+    video_fp = self.video_fp_list[self.video_fp_list_idx]
+    self.edited_annotations[video_fp] = new_annotations
 
 
 if __name__=='__main__':
